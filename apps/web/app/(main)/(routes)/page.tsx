31a4641bd827c35b30d1f0acd19daccad428ec55
Marquis Pierre <marquispierre27@gmail.com>
2026-08-13 22:25:45 -0400

    added everything

---
+++ apps/web/app/(main)/(routes)/page.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@workspace/ui/components/button"
+// import { auth } from '@clerk/nextjs/server'
+
+export default async function Page() {
+  // await auth.protect()
+  return (
+    <div className="flex min-h-svh p-6">
+      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+        <div>
+          <h1 className="font-medium">Project ready!</h1>
+          <p>You may now add components and start building.</p>
+          <p>We&apos;ve already added the button component for you.</p>
+          <h6>THIS IS A PROTECTED ROUTE!!!</h6>
+          <Button className="mt-2">Button</Button>
+        </div>
+        <div className="text-muted-foreground font-mono text-xs">
+          (Press <kbd>d</kbd> to toggle dark mode)
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,48 @@
+import { Geist, Geist_Mono } from "next/font/google"
+import "@workspace/ui/globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@workspace/ui/lib/utils";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+
+
+const font = Geist({subsets:['latin'],variable:'--font-sans'})
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    
+      
+       <ClerkProvider>
+        <html
+             lang="en"
+            suppressHydrationWarning
+             className={cn("antialiased", fontMono.variable, "font-sans", font.variable)}
+           ><body className="h-full flex items-center justify-center">
+          {/* <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header> */}
+          {children}
+              </body>
+          </html>
+        </ClerkProvider>
+      
+  )
+}
