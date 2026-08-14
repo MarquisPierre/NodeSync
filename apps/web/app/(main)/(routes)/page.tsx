d7530cf5fc796d8aef204eb866d7a5b7a98d3a26
Marquis Pierre <marquispierre27@gmail.com>
2026-08-14 00:03:48 -0400

    Added a functional sign-out button on the homepage

--- apps/web/app/(main)/(routes)/page.tsx
+++ apps/web/app/(main)/(routes)/page.tsx
@@ -1,22 +1,14 @@
+'use client'
 import { Button } from "@workspace/ui/components/button"
-// import { auth } from '@clerk/nextjs/server'
+import { useClerk } from '@clerk/nextjs'
 
-export default async function Page() {
-  // await auth.protect()
+export default function Page() {
+    const { signOut } = useClerk()
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <h6>THIS IS A PROTECTED ROUTE!!!</h6>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="text-muted-foreground font-mono text-xs">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
+    // Clicking this button signs out a user
+    // and redirects them to the home page "/".
+    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+    <Button  onClick={() => signOut({ redirectUrl: '/' })}>Sign out</Button>
     </div>
   )
 }
